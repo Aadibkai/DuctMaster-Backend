a1d154ac9667b18e9199f07f9ea172a22a9d0d41
Adib Kazi <adibkazi1106@gmail.com>
2025-07-22 09:58:49 +0530

    c

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -1,9 +1,9 @@
-import Appointment from "../models/appointment.js";
+import appointment from "../models/appointment.js";
 
 export const bookAppointment = async (req, res) => {
   try {
     const { name, email, date } = req.body;
-    const newAppointment = new Appointment({ name, email, date });
+    const newAppointment = new appointment({ name, email, date });
     await newAppointment.save();
     res.status(201).json({ message: 'Appointment booked successfully', appointment: newAppointment });
   } catch (error) {
@@ -12,7 +12,7 @@ export const bookAppointment = async (req, res) => {
 };
 export const getAllAppointments = async (req, res) => {
   try {
-    const appointments = await Appointment.find().sort({ createdAt: -1 }); // Latest first
+    const appointments = await appointment.find().sort({ createdAt: -1 }); // Latest first
     res.status(200).json(appointments);
   } catch (error) {
     res.status(500).json({ message: "Failed to fetch appointments", error });
